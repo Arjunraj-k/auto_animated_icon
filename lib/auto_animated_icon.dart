2a1cc3a6fb8f32410059f4eefb6060ee6a921517
Arjunraj <arjunrajkokkadan@gmail.com>
2019-07-20 01:08:45 +0530

    Formatted code

--- lib/auto_animated_icon.dart
+++ lib/auto_animated_icon.dart
@@ -117,4 +117,3 @@ class _AutoAnimatedIconState extends State<AutoAnimatedIcon>
     );
   }
 }
-
--- test/auto_animated_icon_test.dart
+++ test/auto_animated_icon_test.dart
@@ -6,7 +6,7 @@ import 'package:auto_animated_icon/auto_animated_icon.dart';
 void main() {
   test('create an auto animated icon', () {
     AutoAnimatedIcon(
-      onPressed: (){
+      onPressed: () {
         print("OnPressed Works");
       },
       icon: AnimatedIcons.menu_close,
